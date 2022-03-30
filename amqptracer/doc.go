4e73251893c74b38549dbc6cb2ac7c3e01e85381
Fabian Holler <fabian.holler@simplesurance.de>
2022-03-30 15:50:55 +0200

    use official rabbitmq/amqp091-go driver instead of streadway/amqp
    streadway/amqp is not maintained anymore, the package was forked and is
    continued be maintained by the RabbitMQ team.
    Change the imports to work the official amqp package from the RabbitMQ
    team.

--- amqptracer/doc.go
+++ amqptracer/doc.go
@@ -1,3 +1,6 @@
 // Package amqptracer provides OpenTracing instrumentation for the
-// github.com/streadway/amqp package.
+// github.com/rabbitmq/amqp091-go package.
+//
+// This is a fork of https://github.com/opentracing-contrib/go-amqp that uses
+// use github.com/rabbitmq/amqp091-go instead of github.com/streadway/amqp.
 package amqptracer
